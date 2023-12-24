181b015e45fd92982a4a3267085cf989c17f71e6
Maruthi <bommir467@gmail.com>
2023-12-24 17:22:54 -0600

    signup page

--- app.component.ts
+++ app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, LoginComponent]
+    imports: [CommonModule, RouterOutlet, LoginComponent, SignupComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'upScaling';
